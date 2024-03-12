ae321c6e468281fa520073d11f3542fc4916481c
mubai <mubaiany@gmail.com>
2024-03-12 19:44:49 +0800

    Logistics finish

--- Logistics-Regression/Logistics.py
+++ Logistics-Regression/Logistics.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 """
@@ -52,6 +53,32 @@ def gradAscent(dataMatIn, classLabels):
     return weights.getA()
 
 '''
+改进的随机梯度上升算法
+Parameters：
+    dataMatrix - 数据数组
+    classLabels - 数据标签
+    numIter - 迭代次数
+Returns：
+    weights - 求得的回归系数数组
+'''
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+    m,n = np.shape(dataMatrix)                                                #返回dataMatrix的大小。m为行数,n为列数。
+    weights = np.ones(n)                                                       #参数初始化
+    for j in range(numIter):
+        dataIndex = list(range(m))
+        for i in range(m):
+            alpha = 4/(1.0+j+i)+0.01                                            #降低alpha的大小，每次减小1/(j+i)。
+            randIndex = int(random.uniform(0,len(dataIndex)))                #随机选取样本
+            h = sigmoid(sum(dataMatrix[dataIndex[randIndex]]*weights))         #选择随机选取的一个样本，计算h
+            error = classLabels[dataIndex[randIndex]] - h                        #计算误差
+            weights = weights + alpha * error * dataMatrix[dataIndex[randIndex]]   #更新回归系数
+            del(dataIndex[randIndex])                                         #删除已经使用的样本
+    return weights                                                             #返回
+
+
+
+
+'''
 绘制数据集
 Parameters：
     weights - 权重参数
@@ -82,5 +109,5 @@ def plotBestFit(weights):
 
 if __name__ == "__main__":
     dataMat, labelMat = loadDataSet()
-    weights = gradAscent(dataMat, labelMat)
+    weights = stocGradAscent1(np.array(dataMat), labelMat)
     plotBestFit(weights)
